443b9becd6251d4d1466bb1dd5449c5a89d73b78
sabahzero <bysabahulhasan@gmail.com>
2022-07-22 09:46:14 -0700

    updates

---
+++ Code-Assessment/anagram_palyndrom_finder.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from urllib.request import urlopen
+
+with urlopen( 'https://www.wikidata.org/wiki/Q13417213' ) as webpage:
+    content = webpage.read().decode()
+
+print(content)
