aa30322e29142f2550c48ff5ec3e8dde2209f51a
sabahzero <bysabahulhasan@gmail.com>
2022-07-22 10:38:13 -0700

    notebook updates

--- Code-Assessment/anagram_palyndrom_finder.py
+++ Code-Assessment/anagram_palyndrom_finder.py
@@ -2,7 +2,11 @@
 
 from urllib.request import urlopen
 
-with urlopen('https://www.wikidata.org/wiki/Q13417213') as webpage:
+with urlopen('https://www.wikidata.org/wiki/Q13417213') as webpage: # Adjust to be any URL (selected page with palindromes)
     content = webpage.read().decode()
+    # convert content to be strings in a csv with 3 columns (string, anagram, palindrome)
+    # which strings are anagrams of each other: True of False (update 2nd column of csv)
+    # which strings are palindromes: True or False (update 3rd column of csv)
+    # update to be final csv for easy readability for user (hypothetical of nucleotides and reverse compliments)
 
 print(content)
